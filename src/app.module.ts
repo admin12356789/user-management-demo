d201b3a58ea979283c56a1bdc34907962dd31be5
admin12356789 <148182148+admin12356789@users.noreply.github.com>
2023-10-18 14:32:31 +0700

    update app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,39 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule } from '@nestjs/mongoose'
-import {ConfigModule , ConfigService} from '@nestjs/config'
-import * as Joi from '@hapi/joi'
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema :  Joi.object({
+      validationSchema: Joi.object({
         MONGO_USERNAME: Joi.string().required(),
         MONGO_PASSWORD: Joi.string().required(),
         MONGO_DATABASE: Joi.string().required(),
         MONGO_HOST: Joi.string().required(),
-      })
+      }),
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
         const username = configService.get('MONGO_USERNAME');
         const password = configService.get('MONGO_PASSWORD');
-        const database = configService.get('MONGO_DATABASE') || 'users';
-        const host = configService.get('MONGO_HOST') 
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
 
         return {
-          uri: `mongodb+srv://${username}:${password}@${host}`,
+          // uri: `postgresql://users:${username}:${password}@${host}`,
+          uri: 'postgresql://users:AVNS_HMaXMpLv793N1kwNIMo@app-85df53a3-4246-45be-afaf-dc5c5515b49a-do-user-14829488-0.c.db.ondigitalocean.com:25060/users?sslmode=require',
           dbName: database,
         };
       },
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     UsersModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [],
   providers: [],
 })
-
 export class AppModule {}
