8be8ad8b3de1bf11849f7f84c07d957082e0a417
admin12356789 <148182148+admin12356789@users.noreply.github.com>
2023-10-18 16:01:15 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule } from '@nestjs/mongoose'
-import {ConfigModule , ConfigService} from '@nestjs/config'
-import * as Joi from '@hapi/joi'
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema :  Joi.object({
+      validationSchema: Joi.object({
         MONGO_USERNAME: Joi.string().required(),
         MONGO_PASSWORD: Joi.string().required(),
         MONGO_DATABASE: Joi.string().required(),
         MONGO_HOST: Joi.string().required(),
-      })
+      }),
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
@@ -20,20 +20,21 @@ import { AuthModule } from './auth/auth.module';
         const username = configService.get('MONGO_USERNAME');
         const password = configService.get('MONGO_PASSWORD');
         const database = configService.get('MONGO_DATABASE') || 'users';
-        const host = configService.get('MONGO_HOST') 
+        const host = configService.get('MONGO_HOST');
 
         return {
-          uri:  `mongodb+srv://${username}:${password}@${host}`,
+          uri:
+            configService.get('DATABASE_URL') ||
+            `mongodb+srv://${username}:${password}@${host}`,
           dbName: database,
         };
       },
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     UsersModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [],
   providers: [],
 })
-
 export class AppModule {}
