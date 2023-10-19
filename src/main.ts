9f5fee69df3b43ca38215aef26ce29c83ed6b0ef
admin12356789 <148182148+admin12356789@users.noreply.github.com>
2023-10-19 09:46:12 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('User example')
     .setDescription('The users API description')
